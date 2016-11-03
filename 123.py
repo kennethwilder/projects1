f5564a100b0b13c291529facd4cd37387988eb9b
Kenneth Wilder <kennethwilder@yahoo.com>
2016-11-02 23:26:50 -0400

    My frst commit

---
+++ 123.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import Tkinter
+
+class simpleapp_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        pass
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('my application')
+    app.mainloop()
